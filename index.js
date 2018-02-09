b4bf0111b303b822c763734e5137ab4de07e8123
Matthew Sucharski <a7x1013@aol.com>
2018-02-09 07:39:52 -0500

    copied from my other repo

---
+++ index.js
@@ -0,0 +1,26 @@
+let giphy_api = "http://api.giphy.com/v1/gifs/search";
+
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    $("form [name='offset']").val(0);
+    getAndRenderGifs();
+  });
+
+function getAndRenderGifs() {
+  $.ajax({
+    method: "GET",
+    url: giphy_api,
+    data: $("form").serialize(),
+    success: onSuccess,
+
+  });
+}
+
+function onSuccess(json) {
+  if (json.pagination.offset === 0) {
+    $(".gif-img").remove();
+  }
+  json.data.forEach(function(v,i){
+    $(".gif-gallery").append($("<img class='img-responsive img-thumbnail gif-img' src="+v.images.fixed_height.url+">"));
+  });
+}
